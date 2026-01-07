078881857322a5d54d3194e17931cdfb2ea338c4
gloriousmidnight <ceburke@ualberta.ca>
2026-01-06 18:29:37 -0700

    test

---
+++ PetShop/app/src/main/java/com/example/petshop/Cat.java
@@ -0,0 +1,23 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Cat extends Pet implements Pettable {
+    public Cat(String name) {
+        super(name);
+    }
+
+    public Cat(Date birthDate, String name) {
+        super(birthDate, name);
+    }
+
+    @Override
+    public String speak() {
+        return "meow"; // Cats meow
+    }
+
+    @Override
+    public Void pet() {
+        return null;
+    }
+}
---
+++ PetShop/app/src/main/java/com/example/petshop/Scorpion.java
@@ -0,0 +1,19 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Scorpion extends Pet {
+    public Scorpion(String name) {
+        super(name);
+        this.birthDate = new Date();
+    }
+
+    public Scorpion(Date birthDate, String name) {
+        super(birthDate, name);
+    }
+
+    @Override
+    public String speak() {
+        return "hiss"; // scorpions hiss
+    }
+}
